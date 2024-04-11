848fe8aa8274b1268b29df5d372104112243d50f
Chris-Wilco <christopher.wilcoski@gmail.com>
2024-04-10 17:13:19 -0700

    remove commented out code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "./style.css";
 import * as Page from "./modules/Page.js";
+import * as UI from "./modules/UI.js";
 
-const newPage = Page.generatePage();
+/* const newPage = Page.generatePage(); */
+
+const newPage = new UI(user);
